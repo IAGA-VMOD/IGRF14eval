41bee8e83118545a64a3913d75035d9c6e001391
ancklo <ancklo@space.dtu.dk>
2024-08-28 15:28:42 +0200

    Changed notebooks to allow comparisons to a single candidate model; added grid lines to RMS difference plot.

--- notebooks/DGRF_candidate_comparison.py
+++ notebooks/DGRF_candidate_comparison.py
@@ -55,7 +55,7 @@ from src import shc_utils as shau
 # Set up line colour/type/marker properties using cycler
 from cycler import cycler
 cc = ( cycler(linestyle=['-', '--', '-.',':']) *
-       cycler(color=list('rgbk')) * 
+       cycler(color=list('rgbk')) *
        cycler(marker=['none', 's', '.']))
 
 # %% [markdown]
@@ -84,7 +84,7 @@ Check files are correctly formatted
 """
 print('List of ' + candidate + ' Candidates \n ----------- ')
 files = sorted(glob.glob(DGRF_DIR))
-print(*files, sep='\n')    
+print(*files, sep='\n')
 
 pass_or_fail = shau.check_cof_file_format(files, field_type)
 
@@ -92,8 +92,8 @@ if not(pass_or_fail).all():
     raise Exception('A candidate model format is incorrect. Fix it before \
                     continuing')
 
-# Load in the coefficients - define the type (main or sv) and the leading 
-# acronym on the filename 
+# Load in the coefficients - define the type (main or sv) and the leading
+# acronym on the filename
 [coeffs, institute_name, degree] = \
          shau.load_coeffs_to_numpy(sorted(files), field_type, candidate)
 num_candidates = coeffs.shape[1]
@@ -125,11 +125,11 @@ for label, y in spectra.items():
 # Adding legend, x and y labels, and title for the lines
 ax.legend()
 plt.xlabel('Degree (n)')
-plt.ylabel('R_n (nT^2)') 
+plt.ylabel('R_n (nT^2)')
 ax.grid()
 plt.xticks(np.arange(1, degree+1,2))
 plt.xlim(1,degree)
-plt.title('Mausberger-Lowes Spectrum for ' + candidate + 
+plt.title('Mausberger-Lowes Spectrum for ' + candidate +
           ' ' + year + ' candidates')
 
 
@@ -148,12 +148,12 @@ Second Test: plot of RMS difference between each model (dP)
 dP = np.full((num_candidates,num_candidates),np.nan)
 for i in range(num_candidates):
     for j in range(i+1,num_candidates):
-        dP[i,j] = np.sqrt( 
-                    np.sum( 
-                    np.multiply( 
-                    np.arange(2,degree+2), 
+        dP[i,j] = np.sqrt(
+                    np.sum(
+                    np.multiply(
+                    np.arange(2,degree+2),
                     shau.msumnsq( (coeffs[:,i] - coeffs[:,j])**2, degree) )))
-    
+
 # Print to screen two decimal places
 print('RMS Difference between ' + candidate + ' ' + year + ' candidates')
 pdP = dP.copy()
@@ -161,7 +161,7 @@ pdP[np.isnan(pdP)]=0
 for i in np.around(pdP,2):
     print(" ".join([str(l).rjust(5) for l in i]))
 
-fig, ax = plt.subplots(figsize=(10, 10)) 
+fig, ax = plt.subplots(figsize=(10, 10))
 plt.subplots_adjust(top=0.91, bottom=0.13, left=0.075, right=0.965)
 im = plt.imshow(dP, cmap='cividis_r')
 
@@ -170,6 +170,7 @@ plt.xticks(np.arange(0, num_candidates,1))
 ax.set_xticklabels(institute_name)
 plt.yticks(np.arange(0, num_candidates,1))
 ax.set_yticklabels(institute_name)
+ax.grid(True, ls='ls')
 plt.colorbar(label='nT')
 plt.title('RMS Difference between ' + candidate + ' ' + year + ' candidates')
 plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
@@ -204,20 +205,20 @@ dC_degree = []
 pos = np.array([i for i, elem in enumerate(files) if compare_against in elem])
 
 for j in range(num_candidates):
-    
-        dP_degree.append( np.multiply( 
-                    np.arange(2,degree+2), 
-                    shau.msumnsq( (coeffs[:,pos[0]] - coeffs[:,j])**2, degree) 
+
+        dP_degree.append( np.multiply(
+                    np.arange(2,degree+2),
+                    shau.msumnsq( (coeffs[:,pos[0]] - coeffs[:,j])**2, degree)
                     ) )
-        
-        dC_degree.append(np.divide( 
-                          shau.msumnsq( 
-                          np.multiply(coeffs[:,pos[0]], coeffs[:,j]), degree), 
+
+        dC_degree.append(np.divide(
+                          shau.msumnsq(
+                          np.multiply(coeffs[:,pos[0]], coeffs[:,j]), degree),
                           np.sqrt( np.multiply(
-                              shau.msumnsq( coeffs[:, pos[0]]**2, degree), 
-                              shau.msumnsq( coeffs[:, j]**2, degree  ) ) ) 
+                              shau.msumnsq( coeffs[:, pos[0]]**2, degree),
+                              shau.msumnsq( coeffs[:, j]**2, degree  ) ) )
                           ) )
-    
+
 
 degree_diff = dict(zip(institute_name, dP_degree))
 degree_corr =  dict(zip(institute_name, dC_degree))
@@ -237,9 +238,9 @@ for label, y in degree_diff.items():
    ax1.semilogy(np.arange(1, degree+1), y, label = label)
 # Adding, x and y labels, and title for the lines
 plt.xlabel('Degree (n)')
-ax1.set_ylabel('nT^2') 
+ax1.set_ylabel('nT^2')
 ax1.grid()
-ax1.set_title('Mean Square Difference per degree between ' + compare_against + 
+ax1.set_title('Mean Square Difference per degree between ' + compare_against +
               ' / '+ candidate + ' ' + year + ' candidates')
 
 # Plotting the dC_degree
@@ -247,9 +248,9 @@ for label, y in degree_corr.items():
    ax2.plot(np.arange(1, degree+1), y, label = label)
 # Adding, x and y labels, and title for the lines
 plt.xlabel('Degree (n)')
-ax2.set_ylabel('Correlation') 
+ax2.set_ylabel('Correlation')
 ax2.grid()
-ax2.set_title('Degree correlation between Candidate '+ compare_against + 
+ax2.set_title('Degree correlation between Candidate '+ compare_against +
               ' / ' + candidate + ' ' + year + ' candidates')
 ax2.legend(ncols=2, loc=3)
 plt.xticks(np.arange(1, degree+1, 2))
@@ -264,7 +265,7 @@ plt.xlim(1,degree)
 
 Fourth Test: Plot the azimuthal power spectrum of each candidate
 
-Based on the example from E. Thebault 
+Based on the example from E. Thebault
 
 """
 
@@ -275,7 +276,7 @@ ax.set_prop_cycle(cc)
 # Place the Rn values and institute names in a dictionary
 Az = []
 for j in range(num_candidates):
-    xa, ra = shau.ra_spectrum(degree,1, 1/(degree+1), 
+    xa, ra = shau.ra_spectrum(degree,1, 1/(degree+1),
                               (coeffs[:,pos[0]] - coeffs[:,j]))
     Az.append(ra)
 az_spectra = dict(zip(institute_name, Az))
@@ -286,9 +287,9 @@ for label, y in az_spectra.items():
 # Adding legend, x and y labels, and title for the lines
 ax.legend(ncols=3, loc=1)
 plt.xlabel('order/degree (m/n)')
-plt.ylabel('Azimuthal power (nT^2)') 
+plt.ylabel('Azimuthal power (nT^2)')
 ax.grid()
-plt.title('Azimuthal Spectrum for ' + candidate + 
+plt.title('Azimuthal Spectrum for ' + candidate +
           ' ' + year + ' candidates')
 plt.xlim((-1.01, 1.01))
 
@@ -298,22 +299,22 @@ plt.xlim((-1.01, 1.01))
 # %%
 """
 
-Fifth Test: Plot a series of small triangles showing the main field or SV 
-coefficient differences  against the other candidates 
+Fifth Test: Plot a series of small triangles showing the main field or SV
+coefficient differences  against the other candidates
 
 
 """
 # Define the contour levels to use in imshow
 min_max = 0.4 #nT
 
-# Work out the best layout of the plots in a subfigure e.g. 4 x 4 
+# Work out the best layout of the plots in a subfigure e.g. 4 x 4
 # using the sqrt and remainder
 nrows = round(np.sqrt(len(files)-1))
 ncols = int(np.ceil( (len(files)-1)/nrows))
 
 # Define the figure and each axis for the n rows and m columns
 fig, axs = plt.subplots(nrows=nrows,ncols=ncols,
-                        figsize=(11,8.5))
+                        figsize=(11,8.5), squeeze=False)
 # axs is a 2 dimensional array of `GeoAxes`.  Flatten it into a 1-D array
 axs=axs.flatten()
 
@@ -323,10 +324,10 @@ axis_count = 0
 for j in range(num_candidates):
     # Compute the Gauss coefficient differences
     # skip the pos[0] model (or it will be empty)
-    
-    if j != pos[0]: 
+
+    if j != pos[0]:
         tri_gh = shau.gh2triangle(coeffs[:,pos[0]] - coeffs[:,j], degree)
-        
+
         # Title each subplot with the name of the model
         axs[axis_count].set_title(institute_name[j])
         cs = axs[axis_count].imshow(tri_gh, cmap='RdBu')
@@ -335,15 +336,15 @@ for j in range(num_candidates):
         axs[axis_count].set_yticks(np.arange(0, degree,2))
         axs[axis_count].set_yticklabels(np.arange(1,degree+1,2))
         axs[axis_count].set_ylabel('Degree')
-        
-        # Set the +/- order 
+
+        # Set the +/- order
         axs[axis_count].set_xticks([1,degree+1, degree*2-1])
         axs[axis_count].set_xticklabels([-degree, 0, degree])
         axs[axis_count].set_xlabel('Order')
 
-        axis_count += 1 
+        axis_count += 1
 
-plt.suptitle('Difference between Candidate ' + 
+plt.suptitle('Difference between Candidate ' +
              compare_against +  ' / ' + candidate + ' ' + year + ' candidates')
 
 # Delete the unwanted axes
@@ -367,8 +368,8 @@ cbar=fig.colorbar(cs, cax=cbar_ax,orientation='horizontal', label='nT')
 # %%
 """
 
-Sixth Test: Plot a series of small maps showing the main field or SV 
-differences in a chosen component (default 'Z') against the other candidates 
+Sixth Test: Plot a series of small maps showing the main field or SV
+differences in a chosen component (default 'Z') against the other candidates
 
 Can take several minutes depending on the machine speed
 
@@ -386,7 +387,7 @@ min_max = 20 #nT
 contour = 2 # nT
 clevs=np.arange(-min_max,min_max+1,contour)
 
-# Work out the best layout of the plots in a subfigure e.g. 4 x 4 
+# Work out the best layout of the plots in a subfigure e.g. 4 x 4
 # using the sqrt and remainder
 nrows = round(np.sqrt(len(files)-1))
 ncols = int(np.ceil( (len(files)-1)/nrows))
@@ -402,7 +403,7 @@ RREF = 6371.2 # standard geomagnetic Earth radius (in km)
 # Define the figure and each axis for the 3 rows and 3 columns
 fig, axs = plt.subplots(nrows=nrows,ncols=ncols,
                         subplot_kw={'projection': 'hammer'},
-                        figsize=(11,8.5))
+                        figsize=(11,8.5), squeeze=False)
 
 # axs is a 2 dimensional array of `GeoAxes`.  Flatten it into a 1-D array
 axs=axs.flatten()
@@ -413,13 +414,13 @@ axis_count = 0
 for j in range(num_candidates):
     # Compute the Gauss coefficient differences
     # skip the pos[0] model (or it will be empty)
-    
+
     if j != pos[0]:
         print('axes ' + str(axis_count) + ' ' + str(j) + ': ' + \
               institute_name[j])
         # D. Kerridge's code includes the monopole for SHA computation
         gh = np.append(0., coeffs[:,pos[0]] - coeffs[:,j])
-        
+
         # Compute the maps of differences and save to a dict for ease
         # of plotting an individual element
         Bx, By, Bz = zip(*[shal.shm_calculator(gh,degree,RREF, \
@@ -434,15 +435,15 @@ for j in range(num_candidates):
         # Filled colour plot
         cs=axs[axis_count].pcolormesh(np.radians(longs), np.radians(lats),
                                       elements[component],
-                                      cmap='coolwarm', 
-                                      vmin=clevs.min(), 
+                                      cmap='coolwarm',
+                                      vmin=clevs.min(),
                                       vmax=clevs.max())
         # Add the zero contours
         line_c = axs[axis_count].contour(np.radians(longs), np.radians(lats),
                                          elements[component],
                                          levels=[0], alpha=0.3,
                                          colors=['black'], linewidths=1)
-        
+
         # Title each subplot with the name of the model
         axs[axis_count].set_title(institute_name[j])
 
@@ -455,15 +456,15 @@ for j in range(num_candidates):
                 lat_coast = np.radians([point[1] for point in rec.shape.points[:]])
 
                 axs[axis_count].plot(lon_coast, lat_coast, color='k', linewidth=0.6, alpha=0.2)
-        
+
         axs[axis_count].xaxis.set_ticks(np.radians(np.linspace(-180., 180., num=13)))  # meridians
         axs[axis_count].yaxis.set_ticks(np.radians(np.linspace(-60., 60., num=5)))  # parallels
         axs[axis_count].xaxis.set_major_formatter('')  # remove labels from meridians
         axs[axis_count].grid(False)
-        
-        axis_count += 1 
 
-plt.suptitle('Difference in ' + component + ' between Candidate ' + 
+        axis_count += 1
+
+plt.suptitle('Difference in ' + component + ' between Candidate ' +
              compare_against +  ' / ' + candidate + ' ' + year + ' candidates')
 
 # Delete the unwanted axes
